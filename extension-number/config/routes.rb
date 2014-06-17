c4a764dfaf79af027175aa3af64a0814f4486d14
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-17 13:19:56 +0900

    コントローラー初期作成

--- extension-number/config/routes.rb
+++ extension-number/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'members/index'
+
+  get 'members/show'
+
+  get 'members/new'
+
+  get 'members/edit'
+
+  get 'members/create'
+
+  get 'members/update'
+
+  get 'members/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
