950298878f9605d3091b9dbdb532638f0d136f36
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-17 15:46:38 +0900

    migrate修正

--- extension-number/db/migrate/20140617061419_remove_data_to_members.rb
+++ extension-number/db/migrate/20140617061419_remove_data_to_members.rb
@@ -1,8 +1,14 @@
 class RemoveDataToMembers < ActiveRecord::Migration
-  def change
+  def up
     remove_column :members, :busho, :string
     remove_column :members, :name, :string
     remove_column :members, :yomi, :string
     remove_column :members, :tel, :string
   end
+  def down
+  	remove_column :members, :busho, :string
+    remove_column :members, :name, :string
+    remove_column :members, :yomi, :string
+    remove_column :members, :tel, :string
+  end 
 end
