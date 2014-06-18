d9a9acbb585157426d3434f636eb5abdad835dba
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-18 16:54:15 +0900

    モデルにデータ追加とnewした後にcreateでdbに格納とparamsでフォームの値を取得

--- extension-number/app/controllers/members_controller.rb
+++ extension-number/app/controllers/members_controller.rb
@@ -4,17 +4,22 @@ class MembersController < ApplicationController
   def index
   end
 
+# Numberモデルにある全てのデータを表示
   def show
-    number.find(:all)
+    @members = Number.find(:all)
   end
 
+# Numberモデルにデータを追加
   def new
+    @member = Number.new
   end
 
   def edit
   end
 
+# newをした後にcreateでデータベースに格納する
   def create
+    @member = Number.new(member_params)
   end
 
   def update
@@ -22,10 +27,15 @@ class MembersController < ApplicationController
 
   def destroy
   end
-
+  
+  #重複したところをまとめ
   #def set_member
   #  @member = Member.find(params[:id])
   #end
 
+　#URLから送られてきた値とフォームで入力した値をここに取得
+  #def member_params
+  #  params.require(:member).permit(:busho, :name, :yomi, :tel)
+  #end
 
 end
