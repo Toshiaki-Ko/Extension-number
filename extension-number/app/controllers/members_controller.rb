d0da0bce07a363d2c40105a34a148bf001df59c0
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-20 16:28:02 +0900

    リファクタリング

--- extension-number/app/controllers/members_controller.rb
+++ extension-number/app/controllers/members_controller.rb
@@ -1,25 +1,21 @@
 class MembersController < ApplicationController
-  #before_action :set_member, only: [:show, :edit, :update, :destroy]
-
+ 
   def index
   end
 
-# Numberモデルにある全てのデータを表示
+
   def show
-    @members = Number.find(:all)
   end
 
-# Numberモデルにデータを追加
+
   def new
-    @member = Number.new
   end
 
   def edit
   end
 
-# newをした後にcreateでデータベースに格納する
+
   def create
-    @member = Number.new(member_params)
   end
 
   def update
@@ -27,15 +23,5 @@ class MembersController < ApplicationController
 
   def destroy
   end
-  
-  #重複したところをまとめ
-  #def set_member
-  #  @member = Member.find(params[:id])
-  #end
-
-　#URLから送られてきた値とフォームで入力した値をここに取得
-  #def member_params
-  #  params.require(:member).permit(:department, :name, :pronunciation, :phone_number)
-  #end
 
 end
