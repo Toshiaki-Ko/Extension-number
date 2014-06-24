a3a41351c0b3299c2b760c61112e55aad97d570d
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-24 17:35:26 +0900

    phone_numberをinteger型に変更(cherry-pick)

--- extension-number/db/migrate/20140617052617_create_members.rb
+++ extension-number/db/migrate/20140617052617_create_members.rb
@@ -1,11 +1,18 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
+<<<<<<< HEAD
       t.string :department
       t.string :name
       t.string :pronunciation
       t.string :phone_number
 
+=======
+      t.string :department, :null =>false
+      t.string :name, :null => false
+      t.string :pronunciation, :null => false
+      t.integer :phone_number, :null => false
+>>>>>>> 716763f... phone_numberをstring型からinteger型に変更
       t.timestamps
     end
   end
