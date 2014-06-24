ad43cbe5e3301383d672fa6fe1a054c37d85cb7b
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-24 10:36:44 +0900

    モデルテストファイルの編集(初回)

--- extension-number/test/models/member_test.rb
+++ extension-number/test/models/member_test.rb
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert true
+   end
 end
