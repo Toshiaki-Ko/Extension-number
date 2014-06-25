8b326a4d76f05d3c1f6de3f6e11efb72fe87ccb0
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-25 16:29:18 +0900

    ふりがなカラムのバリデーション(平仮名のみ)

--- extension-number/app/models/member.rb
+++ extension-number/app/models/member.rb
@@ -3,4 +3,8 @@ validates :phone_number, presence: true,
     numericality: { only_integer: true,
        greater_than: 999, less_than: 10000, allow_blank: true },
     uniqueness: true
+
+validates :pronunciation, presence: true,
+  :format => { :with => /\A[\p{hiragana}ー－]+\z/ },
+  uniqueness: false
 end
