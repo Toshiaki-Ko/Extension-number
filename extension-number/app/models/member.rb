2b18bd4e9feb6e39c72efb783c3c009cdd76a955
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-25 15:52:55 +0900

    内線番号カラムのバリデーション(4桁数字のみ・重複不可)

--- extension-number/app/models/member.rb
+++ extension-number/app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ActiveRecord::Base
+validates :phone_number, presence: true,
+    numericality: { only_integer: true,
+       greater_than: 999, less_than: 10000, allow_blank: true },
+    uniqueness: true
 end
