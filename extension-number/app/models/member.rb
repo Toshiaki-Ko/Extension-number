6cc4e62b18c8883e237e928a146fc07795844bd2
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-25 16:34:32 +0900

    内線番号カラムのバリデーション修正(重複可に)

--- extension-number/app/models/member.rb
+++ extension-number/app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ActiveRecord::Base
 validates :phone_number, presence: true,
     numericality: { only_integer: true,
        greater_than: 999, less_than: 10000, allow_blank: true },
-    uniqueness: true
+    uniqueness: false
 
 validates :pronunciation, presence: true,
   :format => { :with => /\A[\p{hiragana}ー－]+\z/ },
