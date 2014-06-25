89b90945ea8cd716d1cbe55b02977b339b995bdf
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-25 10:28:32 +0900

    migrateファイル修正

--- extension-number/db/schema.rb
+++ extension-number/db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140617052617) do
 
-  create_table "numbers", force: true do |t|
+  create_table "members", force: true do |t|
     t.string   "department"
     t.string   "name"
     t.string   "pronunciation"
