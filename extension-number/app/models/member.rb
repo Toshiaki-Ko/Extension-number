9826a8d250e8426ff492e150e8564a9f8bfe2568
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-06-26 11:05:26 +0900

    モデルmember.rb: エラーメッセージの表示順番を変更

--- extension-number/app/models/member.rb
+++ extension-number/app/models/member.rb
@@ -1,12 +1,13 @@
 class Member < ActiveRecord::Base
-validates :phone_number, presence: true,
-    numericality: { only_integer: true,
-       greater_than: 999, less_than: 10000, allow_blank: true },
-    uniqueness: false
+validates :name, presence: true, uniqueness: false
 
 validates :pronunciation, presence: true,
   :format => { :with => /\A[\p{hiragana}ー－]+\z/ },
   uniqueness: false
 
-validates :name, presence: true, uniqueness: false
+validates :phone_number, presence: true,
+    numericality: { only_integer: true,
+       greater_than: 999, less_than: 10000, allow_blank: true },
+    uniqueness: false
+
 end
